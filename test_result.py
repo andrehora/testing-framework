f5255dc4275fdc82a2a93fa9db297eb432a30bd4
vic-dti <victor.yano@dtidigital.com.br>
2025-05-02 18:46:40 -0300

    TestResult class

---
+++ test_result.py
@@ -0,0 +1,81 @@
+class TestResult:
+
+    RUN_MSG = 'run'
+    FAILURE_MSG = 'failed'
+    ERROR_MSG = 'error'
+
+    def __init__(self, suite_name=None):
+        self.run_count = 0
+        self.failures = []
+        self.errors = []
+
+    def test_started(self):
+        self.run_count += 1
+
+    def add_failure(self, test):
+        self.failures.append(test)
+
+    def add_error(self, test):
+        self.errors.append(test)
+
+    def summary(self):
+        return f'{self.run_count} {self.RUN_MSG}, ' \
+               f'{len(self.failures)} {self.FAILURE_MSG}, ' \
+               f'{len(self.errors)} {self.ERROR_MSG}'
+
+
+
+class TestCase:
+
+    def __init__(self, test_method_name):
+        self.test_method_name = test_method_name
+
+    def run(self, result):
+        result.test_started()
+        self.set_up()
+        try:
+            test_method = getattr(self, self.test_method_name)
+            test_method()
+        except AssertionError:
+            result.add_failure(self.test_method_name)
+        except Exception:
+            result.add_error(self.test_method_name)
+        self.tear_down()
+
+    def set_up(self):
+        pass
+
+    def tear_down(self):
+        pass
+
+
+class MyTest(TestCase):
+
+    def set_up(self):
+        print('set_up')
+
+    def tear_down(self):
+        print('tear_down')
+
+    def test_a(self):
+        print('test_a')
+
+    def test_b(self):
+        print('test_b')
+
+    def test_c(self):
+        print('test_c')
+
+
+result = TestResult()
+
+test = MyTest('test_a')
+test.run(result)
+
+test = MyTest('test_b')
+test.run(result)
+
+test = MyTest('test_c')
+test.run(result)
+
+print(result.summary())
